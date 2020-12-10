2bcc020f64736f1ce269dee57184812cab9187f6
Martynas Petuška <martynas.petuska@outlook.com>
2020-12-10 19:30:14 +0200

    A

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { parse } from 'envfile';
 import { EnvFileLoader } from './internal/common';
 
 
@@ -13,31 +12,49 @@ window.env = window.env || {}
 const isBrowser = typeof window !== 'undefined' && typeof process === 'object';
 const isNode = typeof window === 'undefined' && typeof process !== 'object';
 
+const parse = (src: string): Env => {
+	// Try parse envfile string
+	const result: Env = {}
+	const lines = src.toString().split('\n')
+	for (const line of lines) {
+		const match = line.match(/^([^=:#]+?)[=:](.*)/)
+		if (match) {
+			const key = match[1].trim()
+			const value = match[2].trim()
+			result[key] = value
+		}
+	}
+	return result;
+}
+
 const loadEnvObject = async (path: string): Promise<Env | undefined> => {
     let loader: undefined | EnvFileLoader = undefined;
     if (isBrowser && !isNode) {
         const { loadEnvFile } = await import('./internal/browser');
         loader = loadEnvFile;
-    }
-    if (isNode && !isBrowser) {
+    } else if (isNode && !isBrowser) {
         const { loadEnvFile } = await import('./internal/node');
         loader = loadEnvFile;
     }
+    console.log({loader})
     const text = loader && await loader(path);
-    return text ? parse<Env>(text) : undefined;
+    console.log({text, parsed: parse(text || '')});
+    return text ? parse(text) : undefined;
 }
 
 const getGlobalObject = (): typeof window | typeof process | undefined => {
     if (isBrowser && !isNode) {
         return window;
-    }
-    if (isNode && !isBrowser) {
+    } else if (isNode && !isBrowser) {
         return process;
+    } else {
+        return undefined;
     }
 }
 
 export const load = async (path: string = 'application.env'): Promise<Env> => {
     const obj = await loadEnvObject(path);
+    console.log({obj})
 
     const globalObj = getGlobalObject();
     const env: Env = {
--- src/internal/browser.ts
+++ src/internal/browser.ts
@@ -1,4 +1,12 @@
 import { EnvFileLoader } from "./common";
 
 
-export const loadEnvFile: EnvFileLoader = async (path: string) => fetch(path).then(it => it.text());
+export const loadEnvFile: EnvFileLoader = async (path: string) => {
+    const res = await fetch(path);
+    const text = await res.text();
+    if (text.startsWith('<')) {
+        throw new Error("Invalid .env file. Looks like default HTML file.")
+    } else {
+        return text;
+    }
+}
